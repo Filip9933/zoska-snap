813e30fa4d1dec947367d0b33870fd786a28d3d4
Filip9933 <filipmruskovi@gmail.com>
2024-09-26 12:34:57 +0200

    initial

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,8 @@
+import  Typography  from "@mui/material/Typography";
+
+
+export default function About() {
+  return (
+    <Typography> Stranka o mne </Typography>
+  );
+}
---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,8 @@
+import  Typography  from "@mui/material/Typography";
+
+
+export default function TermsConditions() {
+  return (
+    <Typography> Podmienky pouzivania tohto webu</Typography>
+  );
+}
